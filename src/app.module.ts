706319eba89677e757a68da42bf99cb00798bfde
Prince Jaiswal <princejaiswal278@gmail.com>
2026-03-27 12:28:47 +0530

    db sqlite configs changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { ReportsService } from './reports/reports.service';
-import { ReportsController } from './reports/reports.controller';
-import { UsersController } from './users/users.controller';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: "sqlite",
+    database: "db.sqlite",
+    entities: [],
+    synchronize: true
+  }),UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
