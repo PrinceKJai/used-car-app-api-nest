a4998a1f23dfeb7180209c0c6cef0804642df42a
Prince Jaiswal <princejaiswal278@gmail.com>
2026-03-28 13:21:26 +0530

    created services and controler for user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+    constructor(private userService: UsersService){}
+    @Post('signup')
+    createUser(@Body() body: CreateUserDto) {
+        return this.userService.create(body.email, body.password);
+    }
+
+    @Get('/:id')
+    findUser(id: number) {
+        return this.userService.findOne(id);
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectRepository(User)private usersRepo: Repository<User>){}
+
+    async create(email: string, password: string) {
+        const user = this.usersRepo.create({ email, password});
+        await this.usersRepo.save(user);
+    }
+
+    async findOne(id: number) {
+        const user = await this.usersRepo.findOneBy({ id });
+        return user;
+    }
+}
