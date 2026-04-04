a072a0d41dc8205a4733fb2a737cf69b5e30c487
Prince Jaiswal <princejaiswal278@gmail.com>
2026-04-04 18:34:08 +0530

    fixed users service tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -5,8 +5,21 @@ describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
+    const fakeUsersRepo = {
+      create: jest.fn(),
+      find: jest.fn(),
+      findOneBy: jest.fn(),
+      save: jest.fn(),
+      remove: jest.fn(),
+    };
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService, 
+        {
+          provide: 'UserRepository',
+          useValue: fakeUsersRepo
+        }
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
