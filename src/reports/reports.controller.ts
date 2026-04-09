7352f4f694a5942161b6f0094cacf568c906e070
Prince Jaiswal <princejaiswal278@gmail.com>
2026-04-09 19:28:56 +0530

    established a relation between user and reports using oneToMany and ManyToOne

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportsDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,8 +11,7 @@ export class ReportsController {
 
     @UseGuards(AuthGuard)
     @Post()
-    createReport(@Body() body: CreateReportsDto) {
-        console.log("bodybodybody", body);
-        return this.reportsService.create(body);
+    createReport(@Body() body: CreateReportsDto, @CurrentUser() user:User) {
+        return this.reportsService.create(body, user);
     }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,16 +3,16 @@ import { CreateReportsDto } from './dtos/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Report } from './report.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
     constructor(@InjectRepository(Report) private reportsRepo: Repository<Report>) {}
 
-    async create(body: CreateReportsDto) {
+    async create(body: CreateReportsDto, user: User) {
         const report = this.reportsRepo.create(body);
+        report.user = user;
         await this.reportsRepo.save(report);
         return report;
-        // const data = JSON.stringify(body);
-        // return `${data} data received`;
     }
 }
