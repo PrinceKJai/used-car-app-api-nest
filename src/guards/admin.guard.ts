471bf7c2debfd678cf8050a3fa97d741dbae3fac
Prince Jaiswal <princejaiswal278@gmail.com>
2026-04-16 15:06:58 +0530

    created admin guard for protecting routes

--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -1,10 +1,12 @@
 import { CanActivate, ExecutionContext } from "@nestjs/common";
 import { Observable } from "rxjs";
 
-export class AuthGuard implements CanActivate {
+export class AdminGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const request = context.switchToHttp().getRequest();
-        // console.log("request.session.userId", request.session.userId);
-        return !!request.session.userId;
+        if(!request.currentUser) {
+            return false;
+        }
+        return request.currentUser.admin;
     }
 }
